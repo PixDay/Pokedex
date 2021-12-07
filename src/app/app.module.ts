fbb66dbc14076e1fcd4455231a15dc04862d06af
PixDay <adrien.colombier@epitech.eu>
2021-12-07 09:36:39 +0100

    [MODIFY] - Imports Http client module for angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+
+// Angular Material Components
+
+// Personal Components and Pages
+import { TestPageComponent } from './pages/test-page/test-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
