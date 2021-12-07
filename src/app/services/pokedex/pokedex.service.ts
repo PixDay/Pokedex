0d9441fe1c2080860fdbb7452697d612a06ee7d5
PixDay <adrien.colombier@epitech.eu>
2021-12-07 22:03:05 +0100

    [MODIFY] - Make the services fill the pokemon array

--- src/app/services/pokedex/pokedex.service.ts
+++ src/app/services/pokedex/pokedex.service.ts
@@ -1,32 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {catchError, map} from 'rxjs/operators';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { PokemonsService } from '../pokemons/pokemons.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public pokemonService: PokemonsService) { }
 
   getPokedex(region: number, listOfPokemon: Pokemon[]) 
   {
     this.http.get('https://pokeapi.co/api/v2/pokedex/' + region).forEach((data: any) => { 
-      console.log(data);
       let id = 1;
       data.pokemon_entries.forEach((pokemonData: any) => {
-        console.log(pokemonData.pokemon_species.name);
         let pokemon: Pokemon = {
           name: pokemonData.pokemon_species.name,
           image: "",
           id: id,
+          pokemonId: (pokemonData.pokemon_species.url.match(/(\d+)(?!.*\d)/)[0]),
           types: [],
           default_competencies: []
         };
         id++;
         listOfPokemon.push(pokemon);
       });
-    });
+      id = 0;
+      listOfPokemon.forEach(pokemon => {
+        console.log(pokemon.pokemonId)
+        this.pokemonService.getPokemon(pokemon.pokemonId, listOfPokemon, id);
+        id++;
+      })
+    }); 
   }
 }
--- src/app/services/pokemons/pokemons.service.ts
+++ src/app/services/pokemons/pokemons.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,14 @@ export class PokemonsService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemon(id: number) {
-    (this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`).forEach(data => { console.log(data); }));
+  getPokemon(id: number, listOfPokemons: Pokemon[], loopIndex: number) {
+    (this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`).forEach((data: any) => {
+      listOfPokemons[loopIndex].image = data.sprites.front_default;
+      data.types.forEach((types: any) => {
+        listOfPokemons[loopIndex].types.push(types.type.name);
+      })
+      
+      console.log(data); 
+    }));
   }
 }
