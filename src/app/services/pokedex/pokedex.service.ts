0505cf8d9388cf66b08ab0ef6c41253a8baaf153
PixDay <adrien.colombier@epitech.eu>
2021-12-07 17:07:37 +0100

    [MODIFY] - Fill the passed Pokemon array to the pokedex service

--- src/app/services/pokedex/pokedex.service.ts
+++ src/app/services/pokedex/pokedex.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {catchError, map} from 'rxjs/operators';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,23 @@ export class PokedexService {
 
   constructor(private http: HttpClient) { }
 
-  getPokedex(region: number) {
-    (this.http.get('https://pokeapi.co/api/v2/pokedex/' + region).forEach(data => { console.log(data) }));
+  getPokedex(region: number, listOfPokemon: Pokemon[]) 
+  {
+    this.http.get('https://pokeapi.co/api/v2/pokedex/' + region).forEach((data: any) => { 
+      console.log(data);
+      let id = 1;
+      data.pokemon_entries.forEach((pokemonData: any) => {
+        console.log(pokemonData.pokemon_species.name);
+        let pokemon: Pokemon = {
+          name: pokemonData.pokemon_species.name,
+          image: "",
+          id: id,
+          types: [],
+          default_competencies: []
+        };
+        id++;
+        listOfPokemon.push(pokemon);
+      });
+    });
   }
 }
