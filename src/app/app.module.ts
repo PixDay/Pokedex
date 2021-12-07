734db4d344113633eacd9aed4f7eb5a17eb893a8
PixDay <adrien.colombier@epitech.eu>
2021-12-07 17:01:56 +0100

    [MODIFY] - Import Forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -22,6 +23,7 @@ import { RegionsComponent } from './pages/regions/regions.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     FlexLayoutModule,
     HttpClientModule
   ],
